49de81bbe2ab108e085d55acbbecccda923e3d52
ammar-oker <ammar.okar@outlook.com>
2020-04-01 20:57:29 +0300

    fix navigation bar

--- js/script.js
+++ js/script.js
@@ -24,4 +24,22 @@ $(document).ready(function () {
         trigger.toggleClass('is-open');
         $('.navbar-nav').toggleClass('open')
     });
+
+    // fix carousel direction issues
+    $("#home-carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).on("touchmove", function(event){
+            var xMove = event.originalEvent.touches[0].pageX;
+
+            if( Math.floor(xClick - xMove) > 5 ){
+                $(this).carousel('prev');
+            }
+            else if( Math.floor(xClick - xMove) < -5 ){
+                $(this).carousel('next');
+            }
+        });
+        $("#home-carousel").on("touchend", function(){
+            $(this).off("touchmove");
+        });
+    });
 });
